5c15f44970d5e2aff9d15e794bcda50629a2ba1e
Phelix <phelixmbani@gmail.com>
2024-12-23 09:06:17 +0300

    email parser

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import re
+
+class EmailAdresssParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+        
+    def parse(self):
+        emails =re.split(r'[,\s]+', self.addresses.strip())
+        unique_mails = sorted(set(emails))
+        return unique_mails
+    
+emails = "john@doe.com, person@somewhere.org john@doe.com"
+parses = EmailAdresssParser(emails)
+
+print(parses.parse())
